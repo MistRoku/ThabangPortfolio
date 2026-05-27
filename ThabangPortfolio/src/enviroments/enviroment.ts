e76410548759637831c00287d1fa416fc280f22e
Thabang Mokgonyana <thabangmokgonyana@gmail.com>
2026-05-27 10:41:25 +0200

    Added items to enviroment.ts for later posible use

--- ThabangPortfolio/src/enviroments/enviroment.ts
+++ ThabangPortfolio/src/enviroments/enviroment.ts
@@ -1,1 +1,9 @@
-console.log("Hello World!")
+// environment.ts
+export const environment = {
+  production: false,
+  emailJS: {
+    publicKey: 'YOUR_EMAILJS_PUBLIC_KEY',
+    serviceId: 'YOUR_SERVICE_ID',
+    templateId: 'YOUR_TEMPLATE_ID'
+  }
+};
